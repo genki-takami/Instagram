30058d63fb53da20fc8143efa08fa5accb8764d1
genki-takami <takamigenki7@gmail.com>
2020-02-24 02:51:16 +0900

    完成！！

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 髙見元基 on 2020/02/23.
+//  Copyright © 2020 genki.takami. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by 髙見元基 on 2020/02/23.
+//  Copyright © 2020 genki.takami. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    @IBOutlet weak var likeButton: UIButton!
+    
+    @IBOutlet weak var likeLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    // PostDataの内容をセルに表示
+    func setPostData(_ postData: PostData) {
+        // 画像の表示
+        postImageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
+        let imageRef = Storage.storage().reference().child(Const.ImagePath).child(postData.id + ".jpg")
+        postImageView.sd_setImage(with: imageRef)
+
+        // キャプションの表示
+        self.captionLabel.text = "\(postData.name!) : \(postData.caption!)"
+
+        // 日時の表示
+        self.dateLabel.text = ""
+        if let date = postData.date {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm"
+            let dateString = formatter.string(from: date)
+            self.dateLabel.text = dateString
+        }
+
+        // いいね数の表示
+        let likeNumber = postData.likes.count
+        likeLabel.text = "\(likeNumber)"
+
+        // いいねボタンの表示
+        if postData.isLiked {
+            let buttonImage = UIImage(named: "like_exist")
+            self.likeButton.setImage(buttonImage, for: .normal)
+        } else {
+            let buttonImage = UIImage(named: "like_none")
+            self.likeButton.setImage(buttonImage, for: .normal)
+        }
+    }
+    
+}
